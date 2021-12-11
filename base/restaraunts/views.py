ee905b0d98f7092d9267df725b3f1da82312aacd
shokHorizon <kostan0008@gmail.com>
2021-12-11 20:54:15 +0300

    CRUD

--- base/restaraunts/views.py
+++ base/restaraunts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
 
 def restaraunts(request):
     context = {}
@@ -8,11 +9,17 @@ def restaraunts(request):
 
     return render(request, 'restaraunts/restaraunts.html')
 
+@csrf_protect
 def create_restaraunt(request):
     context = {}
 
     if request.method == 'POST':
-        test = request.POST.get('place-location')
-        print(test)
+        name = request.POST.get('title')
+        address = request.POST.get('address')
+        phone = request.POST.get('phone')
+        table_location = request.POST.getlist('place-location')
+        table_persons = request.POST.getlist('persons-number')
+        table_numbers = request.POST.getlist('tables-number')
+        print(name, address, phone, table_location, table_numbers, table_persons)
 
     return render(request, 'restaraunts/create.html', context)
